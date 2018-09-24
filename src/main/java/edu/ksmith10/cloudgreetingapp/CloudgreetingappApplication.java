0c9edf8ee01b22273ba99a36fb4e8ffcf5259800
SmithKatrina <ksmith10@ggc.edu>
2018-09-24 17:59:49 -0400

    First Commit

---
+++ src/main/java/edu/ksmith10/cloudgreetingapp/CloudgreetingappApplication.java
@@ -0,0 +1,12 @@
+package edu.ksmith10.cloudgreetingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudgreetingappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudgreetingappApplication.class, args);
+    }
+}
